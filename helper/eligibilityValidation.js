d026727dc1ef08071683f05bb1d5ea36ce3e055b
Yatin Kalra <yatinkalra@Yatins-MacBook-Air.local>
2023-01-08 22:18:18 +0530

    refactored syntax

--- helper/eligibilityValidation.js
+++ helper/eligibilityValidation.js
@@ -2,40 +2,26 @@ const { IStatus, IOwnershipStatus, IMaritalStatus } = require("../types/index");
 
 const transformRisk = (range = 0) => {
   const regularRange = [1, 2];
-  if (regularRange.includes(range)) {
-    return IStatus.Regular;
-  }
-  if (range >= 3) {
-    return IStatus.Responsible;
-  }
+  if (regularRange.includes(range)) return IStatus.Regular;
+  if (range >= 3) return IStatus.Responsible;
   return IStatus.Economic;
 };
 
 const calculateAgeAndIncomeRange = (range = 0, riskCriteria) => {
   const { income, age } = riskCriteria;
-  if (age < 30) {
-    range -= 2;
-  } else if (age >= 30 && age <= 40) {
-    range -= 1;
-  }
-  if (income > 200000) {
-    range -= 1;
-  }
+  if (age < 30) range -= 2;
+  else if (age >= 30 && age <= 40) range -= 1;
+
+  if (income > 200000) range -= 1;
   return range;
 };
 
 const autoInsuranceEligibility = (range = 0, riskCriteria) => {
   const { vehicle } = riskCriteria;
   const currentYear = new Date().getFullYear();
-
-  if (!vehicle) {
-    return IStatus.Ineligible;
-  }
+  if (!vehicle) return IStatus.Ineligible;
   const yearDifference = Math.abs(currentYear - vehicle.year);
-
-  if (yearDifference <= 5) {
-    range += 1;
-  }
+  if (yearDifference <= 5) range += 1;
   return transformRisk(range);
 };
 
@@ -47,42 +33,24 @@ const disabilityInsuranceEligibility = (range = 0, riskCriteria) => {
     marital_status: maritalStatus,
     dependents,
   } = riskCriteria;
-  if (!income || age > 60) {
-    return IStatus.Ineligible;
-  }
-  if (house.ownership_status === IOwnershipStatus.Mortgaged) {
-    range += 1;
-  }
-  if (dependents) {
-    range += 1;
-  }
-  if (maritalStatus === IMaritalStatus.Married) {
-    range -= 1;
-  }
+  if (!income || age > 60) return IStatus.Ineligible;
+  if (house.ownership_status === IOwnershipStatus.Mortgaged) range += 1;
+  if (dependents) range += 1;
+  if (maritalStatus === IMaritalStatus.Married) range -= 1;
   return transformRisk(range);
 };
 
 const homeInsuranceEligibility = (range = 0, riskCriteria) => {
   const { house } = riskCriteria;
-  if (!house) {
-    return IStatus.Ineligible;
-  }
-  if (house.ownership_status === IOwnershipStatus.Mortgaged) {
-    range += 1;
-  }
-
+  if (!house) return IStatus.Ineligible;
+  if (house.ownership_status === IOwnershipStatus.Mortgaged) range += 1;
   return transformRisk(range);
 };
 
 const lifeInsuranceEligibility = (range = 0, riskCriteria) => {
   const { marital_status: maritalStatus, dependents } = riskCriteria;
-  if (dependents) {
-    range += 1;
-  }
-
-  if (maritalStatus === IMaritalStatus.Married) {
-    range += 1;
-  }
+  if (dependents) range += 1;
+  if (maritalStatus === IMaritalStatus.Married) range += 1;
   return transformRisk(range);
 };
 
